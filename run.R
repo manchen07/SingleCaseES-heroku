18002837c55e62584c81d8b243b8090306541562
Man Chen <mchen277@wisc.edu>
2023-07-24 18:59:09 -0500

    Updated app direction.

--- run.R
+++ run.R
@@ -3,7 +3,7 @@ library(shiny)
 port <- Sys.getenv('PORT')
 
 shiny::runApp(
-  appDir = getwd(),
+  appDir = 'inst/shiny-examples/SCD-effect-sizes',
   host = '0.0.0.0',
   port = as.numeric(port)
 )
